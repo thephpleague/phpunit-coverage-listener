e54046b17b734bda44b1b750778cd5e625f419f0
toopay <toopay@taufanaditya.com>
2013-07-27 00:30:36 +0700

    Increase listener test

--- src/League/PHPUnitCoverageListener/Tests/ListenerTest.php
+++ src/League/PHPUnitCoverageListener/Tests/ListenerTest.php
@@ -1,6 +1,9 @@
 <?php namespace League\PHPUnitCoverageListener\Tests;
 
+use League\PHPUnitCoverageListener\ListenerInterface;
+use League\PHPUnitCoverageListener\PrinterInterface;
 use League\PHPUnitCoverageListener\Listener;
+use League\PHPUnitCoverageListener\Printer\ArrayOut;
 use \PHPUnit_Framework_TestCase;
 
 /**
@@ -22,5 +25,15 @@ class ListenerTest extends PHPUnit_Framework_TestCase
 	{
 		$this->setExpectedException('RuntimeException', 'Invalid printer class');
 		$listener = new Listener(array('printer' => new \stdClass()));
+	}
+
+	public function testIntegrity()
+	{
+		$listener = new Listener(array(
+			'printer' => new ArrayOut
+		), false);
+
+		$this->assertInstanceOf('League\PHPUnitCoverageListener\ListenerInterface', $listener);
+		$this->assertObjectHasAttribute('printer', $listener);
 	}
 }
